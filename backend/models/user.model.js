9dad1ac17676606e1e337099aec2d22ed33d367f
Infernario1130 <infernario11@gmail.com>
2026-01-12 20:50:49 +0530

    feat(user): add password hashing, verification, and JWT token generation

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,4 +1,7 @@
 import mongoose , { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken"
+import "dotenv/config";
 
 const userSchema = new Schema ({
     name : {
@@ -23,5 +26,36 @@ const userSchema = new Schema ({
 } ,
 { timestamps : true}
 )
+
+userSchema.pre("save" , async function() {
+    if (!this.isModified("password")) return ;
+    this.password = await bcrypt.hash(this.password , 10)
+})
+
+userSchema.methods.isPasswordCorrect = async function(password) {
+    return await bcrypt.compare(password , this.password)
+}
+
+userSchema.methods.generateAccessToken = async function() {
+    return await jwt.sign(
+        {
+            _id : this._id
+        } , process.env.ACCESS_TOKEN_SECRET ,
+        {
+            expiresIn : ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken = async function() {
+    return await jwt.sign(
+        {
+            _id : this._id
+        } , process.env.REFRESH_TOKEN_SECRET , 
+        {
+            expiresIn : process.env.REFRESH_TOKEN_EXPIRY
+        } 
+    )
+}
 
 export const User = mongoose.model("User" , userSchema)
